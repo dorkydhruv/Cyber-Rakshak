9541110be8bcbb16ae75f7ada9f6736551abf31f
dorkydhruv <thedhruvsharma@gmail.com>
2024-01-15 02:36:55 +0530

    hehe

--- lib/screens/services/criminal_record.dart
+++ lib/screens/services/criminal_record.dart
@@ -1,5 +1,6 @@
 import 'package:cyber_rakshak/constants.dart';
 import 'package:cyber_rakshak/widgets/buildTextField.dart';
+import 'package:cyber_rakshak/widgets/button.dart';
 import 'package:flutter/material.dart';
 
 class CriminalRecord extends StatelessWidget {
@@ -13,58 +14,66 @@ class CriminalRecord extends StatelessWidget {
         elevation: 0,
         backgroundColor: primaryColor,
       ),
-      body: Column(
-        children: [
-          ListTile(
-            leading: CircleAvatar(
-                radius: 30,
-                child: ClipRRect(
-                    borderRadius: BorderRadius.circular(30),
-                    child: const Image(image: AssetImage("assets/ips.png")))),
-            title: const Text(
-              "Investigation officer ",
-              style: TextStyle(
-                  fontSize: 16, color: Colors.grey, fontFamily: "Poppins"),
-            ),
-            subtitle: const Text(
-              'IPS Shruti',
-              style: TextStyle(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListTile(
+              leading: CircleAvatar(
+                  radius: 30,
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: const Image(image: AssetImage("assets/ips.png")))),
+              title: const Text(
+                "Investigation officer ",
+                style: TextStyle(
+                    fontSize: 16, color: Colors.grey, fontFamily: "Poppins"),
+              ),
+              subtitle: const Text(
+                'IPS Shruti',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Poppins',
+                    fontSize: 28),
+              ),
+              trailing: IconButton(
+                onPressed: () => Navigator.pop(context),
+                icon: const Icon(
+                  Icons.arrow_back,
                   color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Poppins',
-                  fontSize: 28),
-            ),
-            trailing: IconButton(
-              onPressed: () => Navigator.pop(context),
-              icon: const Icon(
-                Icons.arrow_back,
-                color: Colors.white,
-                size: 30,
+                  size: 30,
+                ),
               ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            height: MediaQuery.of(context).size.height * 0.33,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/record.png"),
-                fit: BoxFit.fill,
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              height: MediaQuery.of(context).size.height * 0.33,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/record.png"),
+                  fit: BoxFit.fill,
+                ),
               ),
+              child: const Center(
+                  child: Text(
+                "Criminal Record",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              )),
             ),
-            child: const Center(
-                child: Text(
-              "Criminal Record",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold),
-            )),
-          ),
-          buildContainer("Criminal Name / Organization Name *", 40),
-          buildContainer("Aadhaar Card *", 40),
-          buildContainer("Location ( * for Organization )", 40),
-        ],
+            buildContainer("Criminal Name / Organization Name *", 40),
+            buildContainer("Aadhaar Card *", 40),
+            buildContainer("Location ( * for Organization )", 40),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+              child: CustomButton(
+                str: "Find it",
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
